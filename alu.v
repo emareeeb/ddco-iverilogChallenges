8f67524b5b5243bc0f35b46fd4047c6cc2781433
Areeb Ahmed <132186043+emareeeb@users.noreply.github.com>
2023-11-09 22:04:17 +0530

    spc addition

--- alu.v
+++ alu.v
@@ -1,14 +1,19 @@
 module alu_slice (input wire [1:0] op, input wire i0, i1, cin, output wire o, cout);
    wire t_sumdiff, t_and, t_or, t_andor;
+   
    addsub _i0 (op[0], i0, i1, cin, t_sumdiff, cout);
    and2 _i1 (i0, i1, t_and);
    or2 _i2 (i0, i1, t_or);
    mux2 _i3 (t_and, t_or, op[0], t_andor);
    mux2 _i4 (t_sumdiff, t_andor, op[1], o);
+   
 endmodule
 
+
 module alu (input wire [1:0] op, input wire [15:0] i0, i1,
+            
     output wire [15:0] o, output wire cout);
+   
    wire	[14:0] c;
    alu_slice _i0 (op, i0[0], i1[0], op[0] , o[0], c[0]);
    alu_slice _i1 (op, i0[1], i1[1], c[0], o[1], c[1]);
@@ -26,4 +31,5 @@ module alu (input wire [1:0] op, input wire [15:0] i0, i1,
    alu_slice _i13 (op, i0[13], i1[13], c[12], o[13], c[13]);
    alu_slice _i14 (op, i0[14], i1[14], c[13], o[14], c[14]);
    alu_slice _i15 (op, i0[15], i1[15], c[14], o[15], cout);
+   
 endmodule
